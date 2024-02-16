00b530dce017e76806c5a9097c8c2836fe93ede2
Алексей Куртымов <ssshurupilo@gmail.com>
2024-02-16 06:10:09 +0300

    Добавьте файлы проекта.

---
+++ KurtPrLyam/Pages/AddP.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KurtPrLyam.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddP.xaml
+    /// </summary>
+    public partial class AddP : Page
+    {
+        films Context;
+        public AddP()
+        {
+            InitializeComponent();
+            CountryCB.ItemsSource = App.DB.country.ToList();
+            DirectorCB.ItemsSource = App.DB.director.ToList();
+            films film = new films();
+            Context = film;
+            DataContext = Context;
+            
+        }
+
+        private void AddBT_Click(object sender, RoutedEventArgs e)
+        {
+            if(Context != null)
+            {
+                Context.idCountry = CountryCB.SelectedIndex + 1;
+                Context.idDirector = DirectorCB.SelectedIndex + 1;
+                App.DB.films.Add(Context);
+                App.DB.SaveChanges();
+                MessageBox.Show("Succes!");
+                NavigationService.Navigate(new AddP());
+            }
+        }
+    }
+}
---
+++ KurtPrLyam/Pages/MainP.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KurtPrLyam.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainP.xaml
+    /// </summary>
+    public partial class MainP : Page
+    {
+        
+        public MainP()
+        {
+            InitializeComponent();
+            GenreCB.ItemsSource = App.DB.genre.ToList();
+            DirectorCB.ItemsSource = App.DB.director.ToList();
+            CountryCB.ItemsSource= App.DB.country.ToList();
+        }
+
+        private void Show_Click(object sender, RoutedEventArgs e)
+        {
+            var ff = App.DB.films.ToList();
+            if(GenreCB.SelectedIndex != -1 )
+                ff = ff.Where(a => a.genre.name == GenreCB.Text).ToList();
+            if (NameFilmTB.Text != string.Empty)
+                ff = ff.Where(a => a.name.ToLower().Contains(NameFilmTB.Text.ToLower())).ToList();
+            if(CountryCB.SelectedIndex != -1)
+                ff = ff.Where(a => a.country.name == CountryCB.Text).ToList();
+            if(DirectorCB.SelectedIndex != -1)
+                ff = ff.Where(a => a.director.name == DirectorCB.Text).ToList();
+
+
+
+            FilmListDG.ItemsSource = ff;
+        }
+
+        private void ClearBT_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new MainP());
+        }
+    }
+}
